13db5cce7044c0e051fd83ad04743d75f3301531
bob-lytton <leiyunton@pku.edu.cn>
2019-12-12 23:05:12 +0800

    clear unused files

--- yelp_dataset/data.py
+++ yelp_dataset/data.py
@@ -11,6 +11,10 @@ def load_jsondata_from_file(path):
     return data
 
 def get_id_to_num(json_datas, filtered_list, filtered_name, id_name, multi_value):
+    """
+    read from json_datas and return the position it first appears as its index
+    id for data_id, num for index
+    """
     num_to_id = {}
     id_to_num = {}
     tot = 0
@@ -18,36 +22,43 @@ def get_id_to_num(json_datas, filtered_list, filtered_name, id_name, multi_value
         if data[filtered_name] not in filtered_list:
             continue
         if multi_value:
-            data_ids = [data_id.strip() for data_id in data[id_name].split(',')]
+            # this get a list of data_id(str) with no spaces in the beginning or end
+            data_ids = [data_id.strip() for data_id in data[id_name].split(',')] # strip to delete the spaces in the beginning or end of a line
         else:
             data_ids = [data[id_name]]
         for data_id in data_ids:
             if data_id not in id_to_num:
-                num_to_id[tot] = data_id
-                id_to_num[data_id] = tot
-                tot = tot +1
+                num_to_id[tot] = data_id    # input num, output id
+                id_to_num[data_id] = tot    # input id, output num
+                tot = tot + 1
     return num_to_id, id_to_num
 
 def filter_rare_node(users, businesses, reviews, user_threshold, business_threshold, friend_threshold):
+    """
+    filter the nodes with few activities (reviews in this example)
+    """
     continue_filter = True
     filtered_users = set()
     filtered_businesses = set()
-    while(continue_filter):
+    while continue_filter:  # while()?
         continue_filter = False
         # filter step 1
         users_interact_num = {}
         business_interact_num = {}
         for review in reviews:
-            user_id = review['user_id']
-            business_id = review['business_id']
+            user_id = review['user_id'] # a list
+            business_id = review['business_id'] # a list
             users_interact_num[user_id] = users_interact_num.get(user_id, 0) + 1
             business_interact_num[business_id] = business_interact_num.get(business_id, 0) + 1
+
         filtered_review_users = set(u for u in users_interact_num.keys() if users_interact_num[u]>=user_threshold)
         filtered_review_businesses = set(b for b in business_interact_num.keys() if business_interact_num[b]>=business_threshold)
+        
         if (filtered_users != filtered_review_users) or (filtered_businesses != filtered_review_businesses):
             continue_filter = True
+
         # filter step 2
-        #filter user and business
+        # filter user and business
         user_friends_dict = {}
         for user in users:
             user_id = user['user_id']
@@ -58,19 +69,22 @@ def filter_rare_node(users, businesses, reviews, user_threshold, business_thresh
             filtered_friends = [friend.strip() for friend in user['friends'].split(',') if friend.strip() in filtered_review_users]
             if len(filtered_friends) >= friend_threshold:
                 user_friends_dict[user_id] = filtered_friends
+
         continue_inside = True
-        while (continue_inside):
+        while continue_inside:  # while()?
             friends = {}
             continue_inside = False
             for user, user_friends in user_friends_dict.items():
-                filtered_friends = [friend for friend in user_friends if friend in user_friends_dict]
+                filtered_friends = [friend for friend in user_friends if friend in user_friends_dict]   # friend in user_friends_dict's keys
                 if len(filtered_friends) >= friend_threshold:
                     friends[user] = filtered_friends
                 else:
                     continue_inside = True
             user_friends_dict = deepcopy(friends)
+
         filtered_users = set(user_friends_dict.keys())
         filtered_businesses_list = []
+
         for business in businesses:
             business_id = business['business_id']
             if business_id not in filtered_review_businesses:
@@ -81,19 +95,25 @@ def filter_rare_node(users, businesses, reviews, user_threshold, business_thresh
                 continue
             filtered_businesses_list.append(business_id)
         filtered_businesses = set(filtered_businesses_list)
+
         filtered_review = []
         for review in reviews:
             if (review['user_id'] in filtered_users) and (review['business_id'] in filtered_businesses):
                 filtered_review.append(review)
         reviews = deepcopy(filtered_review)
+
         print(len(list(filtered_users)))
         print(len(list(filtered_businesses)))
         print(len(reviews))
         print('filter loop')
+
     print('filter complete')
     return filtered_users, filtered_businesses, filtered_review
 
 def dataset_split(reviews, userid_to_num, businessid_to_num, train_ratio, valid_ratio, test_ratio):
+    """
+    
+    """
     selected_reviews = []
     for review in reviews:
         if (review['user_id'] not in userid_to_num) or (review['business_id'] not in businessid_to_num):
@@ -103,6 +123,7 @@ def dataset_split(reviews, userid_to_num, businessid_to_num, train_ratio, valid_
         filtered_review['business_id'] = businessid_to_num[review['business_id']]
         filtered_review['rate'] = int(review['stars'])
         selected_reviews.append(filtered_review)
+
     n_reviews = len(selected_reviews)
     test_indices = np.random.choice(range(n_reviews), size=int(n_reviews*test_ratio), replace=False)
     left = set(range(n_reviews))-set(test_indices)
