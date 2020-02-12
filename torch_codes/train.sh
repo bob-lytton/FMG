097afe77927a8111be033d694f8073e07556cedf
bob-lytton <leiyunton@pku.edu.cn>
2020-02-12 18:28:15 +0800

    new result

--- torch_codes/train.sh
+++ torch_codes/train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --negatives=3 --mf-train --cluster --fm-factor=20 --mf-factor=20 > train_output_3
+CUDA_VISIBLE_DEVICES=1 python main.py --negatives=6 --mf-train --cluster --fm-factor=20 --mf-factor=20 > train_output_6
